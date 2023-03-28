0e3f2a9d3510ae268feb0c020275f3de26a9e92d
Manoel Maia <msantos.stefanini@desenbahia.ba.gov.br>
2023-03-28 13:55:59 -0300

    Ajuste de versão no link da Controller Catalogo.

--- APIDadosAbertos/Controllers/CatalogoController.cs
+++ APIDadosAbertos/Controllers/CatalogoController.cs
@@ -73,7 +73,7 @@ namespace APIDadosAbertos.Controllers
 
                 new Conjunto{
 
-                @ref = "https://www.bcb.gov.br/htms/dasfn/canais_atendimento/2.0.0/openapi.json",
+                @ref = "https://www.bcb.gov.br/htms/dasfn/canais_atendimento/2.0.1/openapi.json",
                 nome = "Canais de Atendimento",
                 urlBase = "https://wwws.desenbahia.ba.gov.br/dadosabertos/canaisatendimento",
                 urlVisualizacao = "https://dados.desenbahia.ba.gov.br/",
